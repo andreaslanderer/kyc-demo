04afb694f7fb7b09d8df92b9f27befb1310c5cc6
Andreas Landerer <andreaslanderer@users.noreply.github.com>
2023-10-18 22:13:41 +0200

    Separated prompts for better results - kept old prompt for backwards compatability

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import { OpenAI } from 'langchain/llms/openai'
 import {PromptTemplate} from 'langchain/prompts';
-import { systemPrompt } from './prompt.js'
+import {
+    civilStatusPrompt,
+    personalDetailsPrompt,
+    relationBetweenPartnersPrompt,
+    systemPrompt
+} from './prompt.js'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,22 +22,22 @@ app.use(cors())
 app.set('json spaces', 2)
 app.options('*', cors())
 
-const llm = new OpenAI({
-    modelName: 'gpt-4',
-    temperature: 0.0,
-    openAIApiKey: apiKeyOpenAI
-});
-const prompt = PromptTemplate.fromTemplate(systemPrompt)
+const prompts = [
+    { information: "personalDetails", prompt: PromptTemplate.fromTemplate(personalDetailsPrompt) },
+    { information: "civilStatus", prompt: PromptTemplate.fromTemplate(civilStatusPrompt) },
+    { information: "partnerRelations", prompt: PromptTemplate.fromTemplate(relationBetweenPartnersPrompt) }
+]
 
 app.post('/familySituation', (req, res) => {
     console.log('Calling familySituation endpoint')
     const { text } = req.body
     if (text) {
         try {
-            prompt.format({ text }).then(formattedPrompt =>{
+            const prompt = PromptTemplate.fromTemplate(systemPrompt)
+            prompt.format({ text }).then(formattedPrompt => {
                 console.log('Calling OpenAI service', formattedPrompt)
                 getKyc(formattedPrompt).then(result => {
-                    console.log(`OpenAI service call returned successfully with result: ${result}`)
+                    console.log(`OpenAI service call returned successfully with result`)
                     const match = result.match(jsonPattern);
                     if (match && match.length > 0) {
                         res.send(JSON.parse(match[0]))
@@ -57,6 +62,60 @@ app.post('/familySituation', (req, res) => {
 })
 
 async function getKyc(prompt) {
+    const llm = new OpenAI({
+        modelName: 'gpt-4',
+        temperature: 0.0,
+        openAIApiKey: apiKeyOpenAI
+    })
+    return await llm.predict(prompt);
+}
+
+app.post('/familySituationNew', async (req, res) => {
+    console.log('Calling familySituation endpoint')
+    const { text } = req.body
+
+    try {
+        const promises = prompts.map(async p => {
+            const formattedPrompt = await p.prompt.format({
+                familySituationOfTheClient: text
+            })
+            console.log('Calling OpenAI service', p.information)
+            const result = await getCompletion(formattedPrompt)
+            console.log(`OpenAI service call returned successfully with result`, p.information)
+
+            const match = result.match(jsonPattern);
+            if (match && match.length > 0) {
+                return {
+                    prop: p.information,
+                    value: JSON.parse(match[0])
+                }
+            } else {
+                console.log('No response JSON found, therefore returning an empty object', p.information)
+                return {
+                    prop: p.information,
+                    value: {}
+                }
+            }
+        })
+
+        const resultsArray = await Promise.all(promises)
+        const results = {}
+        resultsArray.forEach(r => results[r.prop] = r.value)
+
+        console.log('Finished calling familySituation endpoint')
+        res.send(results)
+    } catch (e) {
+        console.error(`Error while calling familySituation endpoint`, e)
+        res.status(500).send(e)
+    }
+})
+
+async function getCompletion(prompt) {
+    const llm = new OpenAI({
+        modelName: 'gpt-3.5-turbo-16k-0613',
+        temperature: 0.0,
+        openAIApiKey: apiKeyOpenAI
+    });
     return await llm.predict(prompt);
 }
 
--- prompt.js
+++ prompt.js
@@ -1,5 +1,4 @@
 
-
 const systemPrompt = `
     Please take the text below and extract the following data:
 
@@ -28,4 +27,163 @@ const systemPrompt = `
     Your response must only contain the JSON object and nothing else!
 `
 
-export { systemPrompt }
+const familySituationOfTheClientBackground = `
+    Family situation of the client:
+    {familySituationOfTheClient}
+`
+
+const personalDetailsPrompt = `
+    Instruction: 
+    Based on the background data provided basic personal details of the person.
+    Pay special attention to contradicting information.
+    Your response should be according to the defined output structure.
+    Your response must only contain the JSON object.
+    
+    --- Begin Background ---
+    
+    ${familySituationOfTheClientBackground}
+    
+    --- End Background --- 
+    
+    --- Begin Output Structure ---
+    
+    {{
+        "firstName": "The first name of the person",
+        "lastName": "The last name of the person",
+        "dateOfBirth": "The date of birth of the person; FORMAT: yyyy-mm-dd",
+        "nationality": "The nationality of the person; FORMAT: ISO 2-letter country code, e.g. CH, US",
+        "domicile": "The domicile of the person, FORMAT: ISO 2-letter country code, e.g. CH, US",
+        "reasoning": "Please describe based on which information you came to your conclusion"
+    }}
+    
+    --- End Output Structure ---
+`
+
+const civilStatusPrompt = `
+    Instruction: 
+    Based on the background data provided determine the civil status of the person.
+    Pay special attention to contradicting information.
+    Your response should be according to the defined output structure.
+    Your response must only contain the JSON object.
+    
+    --- Begin Background ---
+    
+    ${familySituationOfTheClientBackground}
+    
+    --- End Background --- 
+    
+    --- Begin Output Structure ---
+    
+    {{
+        "civilStatus": "One of the following values: SINGLE, MARRIED, DIVORCED, WIDOWED, SEPARATED, UNKNOWN",
+        "changeOfCivilStatus": "The date when the civil status changed, e.g. date of marriage, divorce date; FORMAT: yyyy-mm-dd"
+        "reasoning": "Please describe based on which information you came to your conclusion"
+    }}
+    
+    --- End Output Structure ---
+`
+
+const noChildrenPrompt = `
+    Instruction: 
+    Based on the background data provided determine the number of children the person has.
+    Your response should be according to the defined output structure.
+    Your response must only contain the JSON object.
+    
+    --- Begin Background ---
+    
+    ${familySituationOfTheClientBackground}
+    
+    --- End Background --- 
+    
+    --- Begin Output Structure ---
+    
+    {{
+        "noChildren": "Numeric value representing the number of children",
+        "reasoning": "Please describe based on which information you came to your conclusion"
+    }}
+    
+    --- End Output Structure ---
+`
+
+const relationBetweenPartnersPrompt = `
+    Instruction: 
+    Based on the background data provided determine the relationships the person has to other partners.
+    Your response should be according to the defined output structure.
+    Your response must only contain the JSON-array, each object representing a relationship to one partner.
+    
+    --- Begin Background ---
+    
+    ${familySituationOfTheClientBackground}
+    
+    --- End Background --- 
+    
+    --- Begin Output Array ---
+   
+   {{
+        "relations": [
+            {{ 
+                "firstName": "The first name of the related person",
+                "lastName": "The last name of the related person",
+                "id": "The id of the related person; leave empty of not available",
+                "relationType": "One of the following values: SPOUSE, PARENT, CHILD, GRANDPARENT, GRANDCHILD, SIBLING, UNKNOWN",
+                "dateOfBirth": "The date of birth of the related person; FORMAT: yyyy-mm-dd",
+                "nationality": "The nationality of the related person; FORMAT: ISO 2-letter country code, e.g. CH, US",
+                "domicile": "The domicile of the related person, FORMAT: ISO 2-letter country code, e.g. CH, US"
+                "reasoning": "Please describe based on which information you came to your conclusion"
+            }}
+        ]
+    }}
+    
+    --- End Output Array ---
+    
+    --- Begin Example ---
+    
+    Input: 
+    
+    Family situation of the client:
+    Maria Messi is married to Jose. Her son Lionel lives in Miami. Her daughter Angela is a
+    Spanish citizen and lives in Barcelona.
+    
+    Output:
+    {{
+        relations: [
+            {{
+                "partnerName": "Jose Messi",
+                "partnerId": "",
+                "relationType": "SPOUSE",
+                "dateOfBirth": "",
+                "nationality": "",
+                "domicile": "",
+                "reasoning": "Is mentioned in family situation"
+            }},
+            {{
+                "partnerName": "Lionel Messi",
+                "partnerId": "",
+                "relationType": "CHILD",
+                "dateOfBirth": "",
+                "nationality": "",
+                "domicile": "US",
+                "reasoning": "Is mentioned in family situation"
+            }},
+            {{
+                "partnerName": "Angela Messi",
+                "partnerId": "",
+                "relationType": "CHILD",
+                "dateOfBirth": "",
+                "nationality": "ES",
+                "domicile": "ES",
+                "reasoning": "Is mentioned in family situation"
+            }}
+        ]
+    }}
+    
+    --- End Example ---
+`
+
+export {
+    civilStatusPrompt,
+    noChildrenPrompt,
+    personalDetailsPrompt,
+    relationBetweenPartnersPrompt,
+    systemPrompt
+}
