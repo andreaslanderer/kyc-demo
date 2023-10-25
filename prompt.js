d46716b2f6a371c856abf5e07ddd134719481e09
Andreas Landerer <andreaslanderer@users.noreply.github.com>
2023-10-25 08:50:19 +0200

    Optimized family situation prompts, too

--- prompt.js
+++ prompt.js
@@ -1,150 +1,170 @@
 const personalDetailsPrompt = `
-    Instruction: 
-    Based on the background data provided basic personal details of the person.
-    Pay special attention to contradicting information.
-    Your response should be according to the defined output structure.
-    Your response must only contain the JSON object.
-    
-    --- Begin Background ---
-    
-    {background}
-    
-    --- End Background --- 
-    
-    --- Begin Output Structure ---
-    
-    {{
-        "firstName": "The first name of the person; if unknown leave empty, DON'T use the salutation (e.g. Frau, Herr, etc.)",
-        "lastName": "The last name of the person",
-        "dateOfBirth": "The date of birth of the person; FORMAT: yyyy-mm-dd",
-        "nationality": "The nationality of the person; FORMAT: ISO 2-letter country code, e.g. CH, US",
-        "domicile": "The domicile of the person, FORMAT: ISO 2-letter country code, e.g. CH, US",
-        "reasoning": "Please describe based on which information you came to your conclusion"
-    }}
-    
-    --- End Output Structure ---
+**Instruction**: 
+- You will receive background data about an individual.
+- Extract basic **personal details** from this data.
+- Be vigilant for any **contradicting information**.
+- Use the structure provided below for your response.
+- **Do not assume**. If specific details are missing, leave the corresponding fields empty.
+
+**Example**: 
+If the background data says: "John Smith, born on 1990-05-12, holds a US passport and resides in Switzerland."
+Your output might be:
+\`\`\`
+{{
+    "firstName": "John",
+    "lastName": "Smith",
+    "dateOfBirth": "1990-05-12",
+    "nationality": "US",
+    "domicile": "CH",
+    "reasoning": "John Smith, born on 1990-05-12, holds a US passport and resides in Switzerland."
+}}
+\`\`\`
+
+**Expected Output Structure**: 
+\`\`\`
+{{
+    "firstName": "Person's first name (omit salutations like Frau, Herr, etc.)",
+    "lastName": "Person's last name",
+    "dateOfBirth": "Date of birth in 'yyyy-mm-dd' format",
+    "nationality": "Nationality in ISO 2-letter country code format (e.g., CH, US)",
+    "domicile": "Domicile in ISO 2-letter country code format (e.g., CH, US)",
+    "reasoning": "Your rationale for the details provided"
+}}
+\`\`\`
+
+--- Begin Background ---
+
+{background}
+
+--- End Background --- 
 `
 
 const civilStatusPrompt = `
-    Instruction: 
-    Based on the background data provided determine the civil status of the person.
-    Pay special attention to contradicting information.
-    Your response should be according to the defined output structure.
-    Your response must only contain the JSON object.
-    
-    --- Begin Background ---
-    
-    {background}
-    
-    --- End Background --- 
-    
-    --- Begin Output Structure ---
-    
-    {{
-        "civilStatus": "One of the following values: SINGLE, MARRIED, DIVORCED, WIDOWED, SEPARATED, UNKNOWN",
-        "changeOfCivilStatus": "The date when the civil status changed, e.g. date of marriage, divorce date; FORMAT: yyyy-mm-dd"
-        "reasoning": "Please describe based on which information you came to your conclusion"
-    }}
-    
-    --- End Output Structure ---
+**Instruction**: 
+- You will be provided with background data about an individual.
+- Identify the person's **civil status** from this data.
+- Be vigilant for any **contradictory details**.
+- Your output should adhere to the structure below.
+
+**Example**: 
+If the background data says: "Jane, who was previously single, got married to Tom on 2018-06-15."
+Your output might be:
+\`\`\`
+{{
+    "civilStatus": "MARRIED",
+    "changeOfCivilStatus": "2018-06-15",
+    "reasoning": "Jane got married to Tom on 2018-06-15."
+}}
+\`\`\`
+
+**Expected Output Structure**: 
+\`\`\`
+{{
+    "civilStatus": "Civil status in one of the following: SINGLE, MARRIED, DIVORCED, WIDOWED, SEPARATED, UNKNOWN",
+    "changeOfCivilStatus": "Date of change in 'yyyy-mm-dd' format (e.g., date of marriage, date of divorce)",
+    "reasoning": "Your rationale for the provided information"
+}}
+\`\`\`
+
+--- Begin Background ---
+
+{background}
+
+--- End Background --- 
 `
 
 const noChildrenPrompt = `
-    Instruction: 
-    Based on the background data provided determine the number of children the person has.
-    Your response should be according to the defined output structure.
-    Your response must only contain the JSON object.
-    
-    --- Begin Background ---
-    
-    {background}
-    
-    --- End Background --- 
-    
-    --- Begin Output Structure ---
-    
-    {{
-        "noChildren": "Numeric value representing the number of children",
-        "reasoning": "Please describe based on which information you came to your conclusion"
-    }}
-    
-    --- End Output Structure ---
+**Instruction**: 
+- Analyze the provided background data.
+- Identify the **number of children** the person has.
+- Structure your output as described below.
+
+**Example**: 
+If the background data says: "John and his wife have three kids: Mike, Anna, and Lisa."
+Your output might be:
+\`\`\`
+{{
+    "noChildren": "3",
+    "reasoning": "John has three kids: Mike, Anna, and Lisa."
+}}
+\`\`\`
+
+**Expected Output Structure**: 
+\`\`\`
+{{
+    "noChildren": "Numeric value indicating the number of children",
+    "reasoning": "Your basis for the provided count"
+}}
+\`\`\`
+
+--- Begin Background ---
+
+{background}
+
+--- End Background --- 
 `
 
 const relationBetweenPartnersPrompt = `
-    Instruction: 
-    Based on the background data provided determine the relationships the person has to other partners.
-    Your response should be according to the defined output structure.
-    Your response must only contain the JSON-array, each object representing a relationship to one partner.
-    
-    --- Begin Background ---
-    
-    {background}
-    
-    --- End Background --- 
-    
-    --- Begin Output Array ---
-   
-   {{
-        "relations": [
-            {{ 
-                "firstName": "The first name of the related person",
-                "lastName": "The last name of the related person",
-                "id": "The id of the related person; leave empty of not available",
-                "relationType": "One of the following values: SPOUSE, PARENT, CHILD, GRANDPARENT, GRANDCHILD, SIBLING, UNKNOWN",
-                "dateOfBirth": "The date of birth of the related person; FORMAT: yyyy-mm-dd",
-                "dateOfDeath": "In case the related person is already deceased; FORMAT: yyyy-mm-dd"
-                "nationality": "Comma-separated list of nationalities of the related person; FORMAT: ISO 2-letter country code, e.g. CH, US",
-                "domicile": "Comma-separated list of domiciles of the related person, FORMAT: ISO 2-letter country code, e.g. CH, US"
-                "reasoning": "Please describe based on which information you came to your conclusion"
-            }}
-        ]
-    }}
-    
-    --- End Output Array ---
-    
-    --- Begin Example ---
-    
-    Input: 
-    
-    Family situation of the client:
-    Maria Messi is married to Jose. Her son Lionel lives in Miami. Her daughter Angela is a
-    Spanish citizen and lives in Barcelona.
-    
-    Output:
-    {{
-        relations: [
-            {{
-                "partnerName": "Jose Messi",
-                "partnerId": "",
-                "relationType": "SPOUSE",
-                "dateOfBirth": "",
-                "nationality": "",
-                "domicile": "",
-                "reasoning": "Is mentioned in family situation"
-            }},
-            {{
-                "partnerName": "Lionel Messi",
-                "partnerId": "",
-                "relationType": "CHILD",
-                "dateOfBirth": "",
-                "nationality": "",
-                "domicile": "US",
-                "reasoning": "Is mentioned in family situation"
-            }},
-            {{
-                "partnerName": "Angela Messi",
-                "partnerId": "",
-                "relationType": "CHILD",
-                "dateOfBirth": "",
-                "nationality": "ES",
-                "domicile": "ES",
-                "reasoning": "Is mentioned in family situation"
-            }}
-        ]
-    }}
-    
-    --- End Example ---
+**Instruction**: 
+- Examine the provided background data.
+- Identify the **relationships** the person has with other individuals.
+- Ensure your response matches the structure described below.
+
+**Example**: 
+Given the background: "Maria Messi is married to Jose. Her son Lionel lives in Miami. Her daughter Angela is a Spanish citizen and lives in Barcelona."
+Your output might be:
+\`\`\`
+{{
+    "relations": [
+        {{
+            "firstName": "Jose",
+            "lastName": "Messi",
+            "relationType": "SPOUSE",
+            "reasoning": "Maria Messi is mentioned as married to Jose."
+        }},
+        {{
+            "firstName": "Lionel",
+            "lastName": "Messi",
+            "relationType": "CHILD",
+            "domicile": "US",
+            "reasoning": "Mentioned as Maria Messi's son living in Miami."
+        }},
+        {{
+            "firstName": "Angela",
+            "lastName": "Messi",
+            "relationType": "CHILD",
+            "nationality": "ES",
+            "domicile": "ES",
+            "reasoning": "Mentioned as Maria Messi's daughter and a Spanish citizen living in Barcelona."
+        }}
+    ]
+}}
+\`\`\`
+
+**Expected Output Structure**: 
+\`\`\`
+{{
+    "relations": [
+        {{
+            "firstName": "First name of the related individual",
+            "lastName": "Last name of the related individual",
+            "id": "ID of the related individual (if available)",
+            "relationType": "Type of relationship: SPOUSE, PARENT, CHILD, GRANDPARENT, GRANDCHILD, SIBLING, UNKNOWN",
+            "dateOfBirth": "Birth date in 'yyyy-mm-dd' format",
+            "dateOfDeath": "Death date in 'yyyy-mm-dd' format (if applicable)",
+            "nationality": "List of nationalities in ISO 2-letter country code format (e.g., CH, US)",
+            "domicile": "List of domiciles in ISO 2-letter country code format (e.g., CH, US)",
+            "reasoning": "Your rationale for the provided details"
+        }}
+    ]
+}}
+\`\`\`
+
+--- Begin Background ---
+
+{background}
+
+--- End Background --- 
 `
 
 const educationPrompt = `
