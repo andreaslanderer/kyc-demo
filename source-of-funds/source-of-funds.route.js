e46f4c51d61f3efdca1406de6c3dcfb26cc09b6a
ericsalzmann1 <105484931+ericsalzmann1@users.noreply.github.com>
2023-11-20 15:10:44 +0100

    full source of funds profile

--- source-of-funds/source-of-funds.route.js
+++ source-of-funds/source-of-funds.route.js
@@ -1,31 +1,75 @@
 import express from 'express';
 import { PromptTemplate } from "langchain/prompts";
 import { prompt } from "../common/llm.js";
-import { sourceOfFundsPrompt } from "./source-of-funds.prompt.js";
+import {
+    incomeMiscellaneousPrompt,
+    incomeFromBusinessActivitiesPrompt,
+    incomeFromRealEstateSalesPrompt,
+    incomeFromInheritancePrompt,
+    incomeFromSalesInterestPrompt
+} from "./source-of-funds.prompt.js";
 import { cacheMiddleware } from "../common/caching.js";
 
 const router = express.Router();
 
-const sourceOfFunds = {
-    information: "sourceOfFunds",
-    prompt: PromptTemplate.fromTemplate(sourceOfFundsPrompt)
+// Prompt-Objekte
+const incomeMiscellaneous = {
+    information: "incomeMiscellaneous",
+    prompt: PromptTemplate.fromTemplate(incomeMiscellaneousPrompt)
+};
+const incomeFromBusinessActivities = {
+    information: "incomeFromBusinessActivities",
+    prompt: PromptTemplate.fromTemplate(incomeFromBusinessActivitiesPrompt)
+};
+const incomeFromRealEstateSales = {
+    information: "incomeFromRealEstateSales",
+    prompt: PromptTemplate.fromTemplate(incomeFromRealEstateSalesPrompt)
+};
+const incomeFromInheritance = {
+    information: "incomeFromInheritance",
+    prompt: PromptTemplate.fromTemplate(incomeFromInheritancePrompt)
+};
+const incomeFromSalesInterest = {
+    information: "incomeFromSalesInterest",
+    prompt: PromptTemplate.fromTemplate(incomeFromSalesInterestPrompt)
 };
 
-router.post('/sourceOfFundsNew', cacheMiddleware(5), async (req, res) => {
-    console.log("Request received on /sourceOfFundsNew");
-    console.log("Received text for processing:", req.body.text);
-    console.log("Using prompt:", sourceOfFundsPrompt);
-    await process(req, res, 'sourceOfFunds', [sourceOfFunds])
+// Route-Definitionen
+router.post('/incomeMiscellaneous', cacheMiddleware(5), async (req, res) => {
+    await processRequest(req, res, 'sourceOfFunds', [incomeMiscellaneous]);
 });
 
-async function process(req, res, endpointName, promptGroup) {
-    console.log("Process function called with text:", req.body.text);
+router.post('/incomeFromBusinessActivities', cacheMiddleware(5), async (req, res) => {
+    await processRequest(req, res, 'incomeFromBusinessActivities', [incomeFromBusinessActivities]);
+});
+
+router.post('/incomeFromRealEstateSales', cacheMiddleware(5), async (req, res) => {
+    await processRequest(req, res, 'incomeFromRealEstateSales', [incomeFromRealEstateSales]);
+});
+
+router.post('/incomeFromInheritance', cacheMiddleware(5), async (req, res) => {
+    await processRequest(req, res, 'incomeFromInheritance', [incomeFromInheritance]);
+});
+
+router.post('/incomeFromSalesInterest', cacheMiddleware(5), async (req, res) => {
+    await processRequest(req, res, 'incomeFromSalesInterest', [incomeFromSalesInterest]);
+});
+
+// Prozessfunktion
+async function processRequest(req, res, endpointName, promptGroup) {
     const { text } = req.body;
-    if (text) {
-        console.log("Processing with prompt:", promptGroup[0].prompt.template);
-        await prompt(text, res, endpointName, promptGroup);
-    } else {
-        res.status(400).json({ "message": "Missing property: text" });
+    if (!text) {
+        return res.status(400).json({ "message": "Missing property: text" });
+    }
+
+    console.log(`Processing request on '${endpointName}' with text:`, text);
+    try {
+        const response = await prompt(text, res, endpointName, promptGroup);
+        console.log(`Response from '${endpointName}' prompt:`, response);
+        res.json(response);
+    } catch (error) {
+        console.error(`Error during processing '${endpointName}':`, error);
+        res.status(500).json({ error: `Error processing request: ${error.message}` });
     }
 }
 
