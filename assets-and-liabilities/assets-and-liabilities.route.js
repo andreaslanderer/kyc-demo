2e1067e4263df807b480603586f9d41b12bbc548
Andreas Landerer <andreaslanderer@gmail.com>
2023-11-22 11:29:05 +0100

    Refactored and added short-term liabilities

--- assets-and-liabilities/assets-and-liabilities.route.js
+++ assets-and-liabilities/assets-and-liabilities.route.js
@@ -1,19 +1,26 @@
 import express from 'express'
-import {prompt, promptWithBackground} from "../common/llm.js";
+import {promptWithBackground} from "../common/llm.js";
 import {PromptTemplate} from "langchain/prompts";
-import {liquidityPrompt} from "./assets-and-liabilities.prompt.js";
+import {liquidityAssetsPrompt, liquidityLiabilitiesPrompt} from "./assets-and-liabilities.prompt.js";
 
 const router = express.Router()
 
-const liquidity = {
-    information: "liquidity",
-    prompt: PromptTemplate.fromTemplate(liquidityPrompt),
+const liquidityAssets = {
+    information: "assets",
+    prompt: PromptTemplate.fromTemplate(liquidityAssetsPrompt),
     question: `Which liquid assets does the person possess?`,
     entries: 10
 }
 
+const liquidityLiabilities = {
+    information: "liabilities",
+    prompt: PromptTemplate.fromTemplate(liquidityLiabilitiesPrompt),
+    question: `Which short-term liabilities does the person possess?`,
+    entries: 10
+}
+
 router.post('/assetsAndLiabilities/liquidity', async (req, res) => {
-    await process(req, res, [liquidity], 'liquidity')
+    await process(req, res, [liquidityAssets, liquidityLiabilities], 'liquidity')
 })
 
 async function process(req, res, promptGroup, endpointName) {
